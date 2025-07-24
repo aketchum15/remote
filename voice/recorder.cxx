ed8b69f92af18a3c3e172c25fd07ce20d18d761f
Adam Ketchum <ketchua@purdue.edu>
2025-07-24 18:15:35 -0400

    var and link

--- voice/recorder.cxx
+++ voice/recorder.cxx
@@ -86,7 +86,7 @@ void Recorder::alsa_init() {
 void Recorder::opus_init() {
 
     int err = 0;
-    auto enc = opus_encoder_create(srate, 1, OPUS_APPLICATION_VOIP, &err);
+    enc = opus_encoder_create(srate, 1, OPUS_APPLICATION_VOIP, &err);
     if (err != 0) {
         //TODO:throw
     };
