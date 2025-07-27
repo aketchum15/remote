0af5a1863f5f32457546fdae3b3411ee28e6d166
Adam Ketchum <ketchua@purdue.edu>
2025-07-26 23:34:33 -0400

    cleaning

--- voice/recorder.cxx
+++ voice/recorder.cxx
@@ -20,64 +20,43 @@ void Recorder::alsa_init() {
 
     int err = 0;
 
-    if ((err = snd_pcm_open(&capture_handle, snd_device.c_str(), SND_PCM_STREAM_CAPTURE, 0)) < 0)
-    {
+    if ((err = snd_pcm_open(&capture_handle, snd_device.c_str(), SND_PCM_STREAM_CAPTURE, 0)) < 0) {
         std::cerr << "cannot open audio device " << snd_device << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
-
-    if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0)
-    {
+    if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
         std::cerr << "cannot allocate hardware parameter structure " << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
-
-    if ((err = snd_pcm_hw_params_any(capture_handle, hw_params)) < 0)
-    {
+    if ((err = snd_pcm_hw_params_any(capture_handle, hw_params)) < 0) {
         std::cerr << "cannot initialize hardware parameter structure " << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
-
-    if ((err = snd_pcm_hw_params_set_access(capture_handle, hw_params,
-                SND_PCM_ACCESS_RW_INTERLEAVED)) < 0)
-    {
+    if ((err = snd_pcm_hw_params_set_access(capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
         std::cerr << "cannot set access type " << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
-
-    if ((err = snd_pcm_hw_params_set_format(capture_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0)
-    {
+    if ((err = snd_pcm_hw_params_set_format(capture_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
         std::cerr << "cannot set sample format " << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
-
-    if ((err = snd_pcm_hw_params_set_rate_near(capture_handle, hw_params,
-                &srate, 0)) < 0)
-    {
+    if ((err = snd_pcm_hw_params_set_rate_near(capture_handle, hw_params, &srate, 0)) < 0) {
         std::cerr << "cannot set sample rate " << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
-
-    if ((err = snd_pcm_hw_params_set_channels(capture_handle, hw_params, nchan))< 0)
-    {
+    if ((err = snd_pcm_hw_params_set_channels(capture_handle, hw_params, nchan))< 0) {
         std::cerr << "cannot set channel count " << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
-
-    if ((err = snd_pcm_hw_params(capture_handle, hw_params)) < 0)
-    {
+    if ((err = snd_pcm_hw_params(capture_handle, hw_params)) < 0) {
         std::cerr << "cannot set parameters " << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
-
-    if ((err = snd_pcm_prepare(capture_handle)) < 0)
-    {
+    if ((err = snd_pcm_prepare(capture_handle)) < 0) {
         std::cerr << "cannot prepare audio interface for use " << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
-
-    if ((err = snd_pcm_start(capture_handle)) < 0)
-    {
+    if ((err = snd_pcm_start(capture_handle)) < 0) {
         std::cerr << "cannot start soundcard " << "(" << snd_strerror(err) << ", " << err << ")" << "\n";
         //TODO: throw
     }
