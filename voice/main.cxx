df1ddaaf0f133f9d075d9796b6c1771c679d7b8d
Adam Ketchum <ketchua@purdue.edu>
2025-07-28 21:10:34 -0400

    first pass gpio

--- voice/main.cxx
+++ voice/main.cxx
@@ -1,11 +1,13 @@
 #include <array>
+#include <atomic>
 #include <condition_variable>
 #include <functional>
 #include <iostream>
 #include <mutex>
 #include <string>
+#include <sys/poll.h>
 #include <thread>
-#include <wiringPi.h>
+#include <gpiod.hpp>
 
 #include "inc/common.hxx"
 #include "inc/recorder.hxx"
@@ -15,36 +17,50 @@ int32_t packet_size = 0;
 std::condition_variable cv;
 std::mutex mtx;
 
+std::atomic<bool> is_held{false};
 
-void gpio_listener(int32_t rec, int32_t notify) {
-    const int DEBOUNCE_MS = 50;
-    const int NO_TIMEOUT = -1;
+void gpio_listener(std::string &chip_path, int32_t btn, int32_t led) {
+    const auto DEBOUNCE_MS = std::chrono::milliseconds(10);
+    auto chip = ::gpiod::chip(chip_path);
 
-    wiringPiSetupGpio();
-    pinMode(rec, INPUT);
+    // This is crazy and awesome 
+    auto btn_request = chip.prepare_request()
+        .set_consumer("button consumer")
+        .add_line_settings(
+            btn,
+            ::gpiod::line_settings()
+                .set_direction(::gpiod::line::direction::INPUT)
+                .set_edge_detection(::gpiod::line::edge::BOTH)
+                .set_bias(::gpiod::line::bias::PULL_UP)
+                .set_debounce_period(DEBOUNCE_MS)
+        )
+    .do_request();
 
-    pullUpDnControl(rec, PUD_UP);
-    pinMode(notify, OUTPUT);
-    digitalWrite(notify, LOW);
 
-    bool toggle = false;
-    auto callback = [notify](struct WPIWfiStatus status, void *user_data) {
-        if (status.edgeType == INT_RISING_EDGE) {
-            digitalWrite(notify, HIGH);
-            // send signal
-        } else {
-            digitalWrite(notify, low);
-            // send end signal
-        }
-    };
-    wiringPiISR2(rec, INT_EDGE_BOTH, &callback, DEBOUNCE_MS, NULL);
+    auto led_request = chip.prepare_request()
+        .set_consumer("led consumer")
+        .add_line_settings(
+            led,
+            ::gpiod::line_settings()
+                .set_direction(::gpiod::line::direction::OUTPUT))
+    .do_request();
 
+    ::gpiod::edge_event_buffer buffer(1);
     for (;;) {
-        //while not die
-    };
-
-    wiringPiISRStop(rec);
-    pinMode(notify, INPUT);
+        btn_request.read_edge_events(buffer);
+        for (const auto &event : buffer) {
+            switch (event.type()) {
+                case ::gpiod::edge_event::event_type::RISING_EDGE:
+                    led_request.set_value(led, ::gpiod::line::value::ACTIVE);
+                    //signal start
+                break;
+                case ::gpiod::edge_event::event_type::FALLING_EDGE:
+                    led_request.set_value(led, ::gpiod::line::value::INACTIVE);
+                    //signal stop
+                break;
+            }
+        }
+    }
 };
 
 void recorder(std::array<uint8_t, MAX_PACKET_SIZE>& buf) {
@@ -84,11 +100,12 @@ void sender(std::array<uint8_t, MAX_PACKET_SIZE>& buf) {
 int main(void) {
     std::array<uint8_t, MAX_PACKET_SIZE> buf = {0};
 
-    std::thread record_thread(recorder, std::ref(buf));
-    std::thread send_thread(sender, std::ref(buf));
+    //std::thread record_thread(recorder, std::ref(buf));
+    //std::thread send_thread(sender, std::ref(buf));
     std::thread gpio_thread(gpio_listener, 4, 2);
 
-    record_thread.join();
-    send_thread.join();
+    //record_thread.join();
+    //send_thread.join();
+    gpio_thread.join();
 
 }
